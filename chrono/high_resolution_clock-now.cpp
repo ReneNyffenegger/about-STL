23e6bc8cda7a4a08c44944ba8efcf80a74313dbc
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2017-07-04 11:07:09 +0200

    + high_resolution_clock-now.cpp

---
+++ chrono/high_resolution_clock-now.cpp
@@ -0,0 +1,22 @@
+#include <chrono>
+#include <iostream>
+#include <thread>
+
+int main() {
+
+
+  auto t0 = std::chrono::high_resolution_clock::now();
+
+  std::cout << "Filling time between t0 and t1" << std::endl;
+
+  std::this_thread::sleep_for(std::chrono::milliseconds(17));
+
+  auto t1 = std::chrono::high_resolution_clock::now();
+
+  std::chrono::duration<float> t_diff = t1 - t0;
+
+  std::chrono::milliseconds t_diff_ms = std::chrono::duration_cast<std::chrono::milliseconds>(t_diff);
+
+  std::cout << "t_diff_ms = " << t_diff_ms.count() << std::endl;
+
+}
